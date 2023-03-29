dfa7fd5884de696b4bf846283811582d33fb9d2b
Vincasmen <vincasmen13@gmail.com>
2023-03-29 23:13:37 +0200

    nav Bar setup

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -17,16 +17,18 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   );
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
-  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
 
   return (
-    <nav className={"z-40 w full fixed top-0 py-6"}>
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
-        <h4 className="font-playfair text-3xl font-bold">JE</h4>
-        {/*DESKTOP NAV*/}
-        {isAboveSmallScreens ? (
+        <h4 className="font-playfair text-3xl font-bold">JV</h4>
+
+        {/* DESKTOP NAV */}
+        {isDesktop ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
             <Link
               page="Home"
@@ -59,8 +61,7 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
             className="rounded-full bg-red p-2"
             onClick={() => setIsMenuToggled(!isMenuToggled)}
           >
-            {" "}
-            <img alt="menu-icon" src="../assets/menu-icon.svg"></img>
+            <img alt="menu-icon" src="../assets/menu-icon.svg" />
           </button>
         )}
       </div>
